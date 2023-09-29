0d1ad562d73ba2e08f30feda398658c58041803f
YugeshKK <95625392+YugeshKK@users.noreply.github.com>
2023-09-29 13:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+
 function getFormvalue() {
     //Write your code here
-
+const first=document.getElementById('first')	
+const last=document.getElementById('last');
+	window.alert(first.value + " "+ last.value);
 }
